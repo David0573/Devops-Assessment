b618a3889ad038e1bd917d2845a11f11943611cc
David0573 <127502829+David0573@users.noreply.github.com>
2025-10-07 23:33:01 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello from Next.js + Docker + GitHub Actions + GHCR!</h1>
+    </div>
+  );
+}
